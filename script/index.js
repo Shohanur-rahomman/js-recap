cea70b08f89f89793d9c182f351d5faf2293ad18
Shohanur-rahomman <mdshohanur757@gmail.com>
2025-03-20 18:47:15 +0600

    first commit

---
+++ script/index.js
@@ -0,0 +1,19 @@
+
+// conditional operation
+const number1 = 1;
+const number2 = 2;
+
+if (number1 > number2) {
+    console.log('number1 greater then number2');
+} else {
+    console.log('number1 not greater then number2');
+};
+// less then check
+const num1 = 1;
+const num2 = 2;
+
+if (num1 < num2) {
+    console.log('num1 less then num2');
+} else {
+    console.log('num1 not less then num2');
+};
